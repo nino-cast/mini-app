a450c4257263e9b31168f166591c9fa829cdc5d0
nino <93901798+nino-cast@users.noreply.github.com>
2021-11-27 12:54:07 +0900

    custom container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '24px',
+      },
+    },
     extend: {
       fontFamily: {
         logo: ['Gloria Hallelujah', 'cursive'],
